6bfa3e868f066b8fd3307d1f79ab4f10ec5081fa
huanghu01 <1525273035@qq.com>
2026-07-26 16:35:32 +0800

    成功code优化

--- src/main/java/com/personrecipe/common/ApiResponse.java
+++ src/main/java/com/personrecipe/common/ApiResponse.java
@@ -9,12 +9,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ApiResponse<T> {
 
+	/**
+	 * 业务成功码。
+	 */
+	public static final int SUCCESS_CODE = 200;
+
 	private int code;
 	private String message;
 	private T data;
 
 	public static <T> ApiResponse<T> ok(T data) {
-		return new ApiResponse<>(0, "ok", data);
+		return new ApiResponse<>(SUCCESS_CODE, "ok", data);
 	}
 
 	public static <T> ApiResponse<T> fail(int code, String message) {
